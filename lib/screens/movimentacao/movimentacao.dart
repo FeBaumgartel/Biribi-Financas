abddfbf788b3cc35f4b6748b6ca511c5ebe5e654
Rangelsk <rangelrobertomachado@gmail.com>
2020-06-26 21:43:02 -0300

    inicio da tela movimentaçoes

---
+++ lib/screens/movimentacao/movimentacao.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Movimentacao extends StatelessWidget{
+  @Override
+  Widget build(BuildContext context){
+    return new MovimentacaoPage();
+  }
+}
+
+class MovimentacaoPage extends StatefulWidget {
+    MovimentacaoPage({Key key}) : super(key: key);
+  
+  @Override 
+  _MovimentacaoPageState createState => _MovimentacaoPageState(extends State<MovimentacaoPage>);
+    
+  }
+  
+  class _MovimentacaoPageState extends State<MovimentacaoPage>{
+  }
+}
